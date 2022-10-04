c8d3881865ef3a53ebbecb3f7a77a6c2c574579a
unknown <Admin@BG1NB1509.SONATA.LOCAL>
2022-10-04 17:32:05 +0530

    Initial Version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MyReactiveFormComponent } from './my-reactive-form/my-reactive-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'Reactive', component : MyReactiveFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
